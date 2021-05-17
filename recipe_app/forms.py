5139c0986db7afda180027a4e699ee8fe8b1b151
Danielle Bobb <danielle@oakandivymedia.com>
2021-05-17 13:07:22 -0700

    establishes forms branch and forms.py which adds Author and Recipe forms

---
+++ recipe_app/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from recipe_app.models import Author
+
+# Create two forms: RecipeForm & AuthorForm
+"""
+Author:
+    - Name: CharField
+    - Bio: TextField
+
+Recipe:
+    - Title: CharField
+    - Author: ForeignKey
+    - Description: TextField
+    - Time Required: CharField (for example, "One hour")
+    - Instructions: TextField
+"""
+
+class Author(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = [
+            'name', 
+            'bio'
+        ]
+
+class AddRecipeForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    author = forms.ModelChoiceField(queryset=Author.objects.all())
+    description = forms.TextField(max_length=500)
+    time_required = forms.CharField(max_length=50)
+    instructions = forms.TextField(widget=forms.Textarea)
