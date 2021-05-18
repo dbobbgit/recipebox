5f6690be27bab63971043c7b0b17e586ec99e30d
Danielle Bobb <danielle@oakandivymedia.com>
2021-05-18 09:01:37 -0700

    makes migrations with updated Author user model

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -5,19 +5,19 @@ from recipe_app.models import Author, Recipe
 from recipe_app.forms import AddAuthorForm, AddRecipeForm
 from django.contrib.auth.forms import UserCreationForm
 
-def index(request):
+def index(req):
     recipes = Recipe.objects.all()
-    return render(request, 'index.html', {'recipes': recipes})
+    return render(req, 'index.html', {'recipes': recipes})
 
-def recipe_detail(request, recipe_id: int):
+def recipe_detail(req, recipe_id: int):
     recipe = Recipe.objects.get(id=recipe_id)
-    return render(request, 'recipe_detail.html', {'recipe': recipe})
+    return render(req, 'recipe_detail.html', {'recipe': recipe})
 
-def author_detail(request, author_id: int):
+def author_detail(req, author_id: int):
     my_author = Author.objects.get(id=author_id)
     author_recipes = Recipe.objects.filter(author=my_author)
     print(author_recipes[0].author) 
-    return render(request, 'author_detail.html', {'author': my_author, 'recipes': author_recipes})
+    return render(req, 'author_detail.html', {'author': my_author, 'recipes': author_recipes})
 
 def add_recipe(req):
     html = 'generic_form.html'
@@ -51,7 +51,7 @@ def add_author(req):
 
     return render(req, html, {'form': form})
 
-def register(req):
-    form = UserCreationForm()
-    return render(req, 'register.html', {'form': form})
-    
+# def register(req):
+#     form = UserCreationForm()
+#     return render(req, 'register.html', {'form': form})
+
